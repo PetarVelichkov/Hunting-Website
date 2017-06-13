a7ac1383bee27725a7f2c6b9ea7793332620eb06
Petar Velichkov <petio.kv@gmail.com>
2017-06-13 12:41:22 +0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,6 @@ module.exports = {
     production: {
         rootPath: rootPath,
         db: process.env.MONGODB_ADDON_URI,
-        port: process.env.port
+        port: process.MONGODB_ADDON_PORT
     }
 };
